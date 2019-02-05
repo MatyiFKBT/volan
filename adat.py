cad2586332887ec8d85ff5ad10fdc53f82dd1712
Bence <b9nc9@protonmail.com>
2019-02-05 21:38:45 +0100

    ÚJ MENETRENDEK DB!
    Az új felépítésű db-vel úgy látom, hogy az átszállásokat meg tudom oldani; valamint hamarosan felhasználóbarátabb lehet a lekérés.
    (Bár szerintem rajtam kívül már/még nem használja ezt senki.)
    A paramétereket ugyanúgy kell kimásolni a böngészőből, ahogy eddig. Annyi különbséggel, hogy annak is más a felépítése. Ettől függetlenül a README-t nem frissítem, mert másolni ugyanúgy kell.

--- adat.py
+++ adat.py
@@ -3,31 +3,24 @@
 import json
 import requests
 
-payload = []
+
+params = []
 with open("param.txt", "r") as keresek:
     sor = keresek.read().split("\n")
     for x in sor:
         if x != "":
-            payload.append(json.loads(x))
+            params.append({'json' : x })
 
 nyers = []
 with requests.Session() as s:
-    for x in payload:
-        p = s.post('https://menetrendek.hu/uj_menetrend/hu/talalatok_json.php', data=x)
-        nyers.append(json.loads(p.content))
+    for x in range(0, len(params)):
+        valasz = s.post('https://menetrendek.hu/menetrend/interface/index.php', data=params[x])
+        nyers.append(json.loads(valasz.content))
+
 
 db = open("adb.txt", "a")
-for x in nyers:
-    for y in range(1,len(x["talalatok"])+1):
-        if ',' in x["talalatok"][str(y)]["departureStation"] and x["talalatok"][str(y)]["departureCity"] == "":
-            felbont = x["talalatok"][str(y)]["departureStation"].split(',')
-            x["talalatok"][str(y)]["departureCity"] = felbont[0]
-            x["talalatok"][str(y)]["departureStation"] = felbont[1].strip()
-        if ',' in x["talalatok"][str(y)]["arrivalStation"] and x["talalatok"][str(y)]["arrivalCity"] == "":
-            felbont = x["talalatok"][str(y)]["arrivalStation"].split(',')
-            x["talalatok"][str(y)]["arrivalCity"] = felbont[0]
-            x["talalatok"][str(y)]["arrivalStation"] = felbont[1].strip()
-            
-        db.write(x["talalatok"][str(y)]["departureCity"] + ";"  + x["talalatok"][str(y)]["departureStation"] + ";" + x["talalatok"][str(y)]["arrivalCity"] + ";" + x["talalatok"][str(y)]["arrivalStation"] + ";" + x["talalatok"][str(y)]["indulasi_ido"] + ";" + x["talalatok"][str(y)]["erkezesi_ido"] + ";" + x["talalatok"][str(y)]["talalat_kozlekedik"] + "\n")
-
+for x in range(0, len(nyers)):
+    for y in range(1, len(nyers[x]["results"]["talalatok"])):
+        busz = nyers[x]["results"]["talalatok"][str(y)]
+        db.write(busz["departureCity"] + ";" + busz["departureStation"] + ";" + busz["arrivalCity"] + ";" + busz["arrivalStation"] + ";" + busz["indulasi_ido"] + ";" + busz["erkezesi_ido"] + ";" + busz["talalat_kozlekedik"] + "\n")
 db.close()
